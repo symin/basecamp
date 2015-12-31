1ead01f13506092a7d58a746cc33e305ed2f8064
syMin <syMin@syMin-PC>
2015-12-31 16:15:13 +0900

    REL

--- boardProject/src/main/java/com/board/boardProject/EmailValidator.java
+++ boardProject/src/main/java/com/board/boardProject/EmailValidator.java
@@ -3,6 +3,15 @@ package com.board.boardProject;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * <pre>
+ * 		서버단에서 이메일 형식 체크를 위한 클래스 
+ * </pre>
+
+ * @Author syMin
+ * @Date 2015. 12. 30.
+ *
+ */
 public class EmailValidator {
 
 	private Pattern pattern;
@@ -16,6 +25,14 @@ public class EmailValidator {
 		pattern = Pattern.compile(EMAIL_PATTERN);
 	}
 
+	/**
+	 * <pre>
+	 * 		이메일 형식이 올바른지 체크 
+	 * </pre>
+	
+	 * @Author : syMin
+	 * @Date : 2015. 12. 30.
+	 */
 	public boolean validate(final String hex) {
 
 		matcher = pattern.matcher(hex);
